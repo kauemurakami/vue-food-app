97c91e9a2db1b535e2721e6c9f3963e2c7cf4e0c
Kauê <kauetmurakami@gmail.com>
2024-08-20 18:00:20 -0300

    estrutura inicial, iniciando funcionalidades

---
+++ scripts.js
@@ -0,0 +1,75 @@
+var products = [
+	{
+		"photo": "img/big-mac.png",
+		"name": "Big Mac",
+		"price": 6.99,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/mc-chicken.png",
+		"name": "Mc Chicken",
+		"price": 4.99,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/double-cb.png",
+		"name": "Double Cheese Burger",
+		"price": 2.99,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/fries.png",
+		"name": "Batata frita",
+		"price": 2.99,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/nuggets.png",
+		"name": "Mc Nuggets",
+		"price": 3.49,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/salad.png",
+		"name": "Salada",
+		"price": 2.79,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/cola.png",
+		"name": "Coca Cola",
+		"price": 1.99,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/lipton.png",
+		"name": "Ice Tea",
+		"price": 1.99,
+		"active": false,
+		"quantity": 1
+	},
+	{
+		"photo": "img/water.png",
+		"name": "Água",
+		"price": 1.49,
+		"active": false,
+		"quantity": 1
+	}
+];
+
+const SelfServiceMachine = { // o que carrega toda logica da aplicação
+	//data onde guardamos os dados que vamos utilizar
+	data() {
+		return { products: window.products }
+	},
+	
+}
+
+Vue.createApp(SelfServiceMachine).mount('#app') //seletor css e o id app <main id="app"> no index html
